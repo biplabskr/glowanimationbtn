ae0ca0389151ab9c9937a0710d22d1c666efe62a
Biplab Sarkar <biplabskr24@gmail.com>
2023-11-21 16:44:47 +0530

    glowanimationbtn

---
+++ glowanimation/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GlwbtnAnimation from './GlowAnimation';
+
+function App() {
+  return (
+    <>
+    <GlwbtnAnimation/>
+    </>
+  );
+}
+
+export default App;
---
+++ glowanimation/src/GlowAnimation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./GlwbtnAnimation.css"
+
+export default function GlwbtnAnimation() {
+    return (
+        <div className='container'>
+
+            <a href='#'>
+
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Button
+
+            </a>
+        </div>
+    )
+}
